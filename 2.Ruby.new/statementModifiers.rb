ff94eb1fa01059bfabfeeabc23f43c38d5cf82ea
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2021-01-28 14:39:59 -0600

    Added statementModifiers.rb

---
+++ 2.Ruby.new/statementModifiers.rb
@@ -0,0 +1,24 @@
+#Example with an if
+
+if radiation > 3000
+  puts "Danger, Will Robinson"
+end
+
+#Using a statement modifier:
+
+puts "Danger, Will Robinson" if radiation > 3000
+
+###############################
+
+
+#Example with a while
+
+square = 4
+while square < 1000
+  square = square * square
+end
+
+#Using a statement modifier:
+
+square = 4
+square = square*square while square < 1000
