4204f4f2ff9a9d70e0885145699d2a28e57fe8d1
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2021-01-29 12:08:39 -0600

    Using attr_accessor

--- 3. Classes, Objects and Variables/BookInStock.rb
+++ 3. Classes, Objects and Variables/BookInStock.rb
@@ -1,4 +1,6 @@
 class BookInStock
+  attr_reader :isbn
+  attr_accessor :price  
   def initialize(isbn, price)
     @isbn = isbn
     @price = price
@@ -6,15 +8,14 @@ class BookInStock
   def to_s
     "ISBN: #{@isbn}, price: #{@price}"
   end
-  attr_reader :isbn, :price  
+  def price=(new_price)
+    @price = new_price
+  end
 end
 
-b1 = BookInStock.new("isbn1", 3)
-puts "ISBN of b1: #{b1.isbn}"
-puts "Price of b1: #{b1.price}"
-b2 = BookInStock.new("isbn2", 3.14)
-puts b2
-b3 = BookInStock.new("isbn3", "5.67")
-puts b3
-
-
+book = BookInStock.new("isbn1", 33.80)
+puts "ISBN= #{book.isbn}"
+puts "Price= #{book.price}"
+book.price = book.price * 0.75
+# discount price
+puts "New price = #{book.price}"
