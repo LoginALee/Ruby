5329a97cf7b88fe51402827e4f7db9a75c6ed5eb
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2021-01-29 10:42:00 -0600

    Added to_s method to BookInStock class

--- 3. Classes, Objects and Variables/classes1.rb
+++ 3. Classes, Objects and Variables/classes1.rb
@@ -3,11 +3,14 @@ class BookInStock
     @isbn = isbn
     @price = price
   end
+  def to_s
+    "ISBN: #{@isbn}, price: #{@price}"
+  end
 end
 
 b1 = BookInStock.new("isbn1", 3)
-p b1
+puts  b1
 b2 = BookInStock.new("isbn2", 3.14)
-p b2
+puts b2
 b3 = BookInStock.new("isbn3", "5.67")
-p b3
+puts b3
