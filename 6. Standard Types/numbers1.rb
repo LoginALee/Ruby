e56e1cf87c2c613cd60fbec04f502f6855453a02
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2021-02-03 14:22:26 -0600

    First chap 6 files

---
+++ 6. Standard Types/numbers1.rb
@@ -0,0 +1,4 @@
+Rational(3, 4) * Rational(2, 3) # => (1/2)
+Rational("3/4") * Rational("2/3") # => (1/2)
+Complex(1, 2) * Complex(3, 4) # => (-5+10i)
+Complex("1+2i") * Complex("3+4i")# => (-5+10i)
---
+++ 6. Standard Types/strings1.rb
@@ -0,0 +1,6 @@
+'escape using "\\"' # => escape using "\"
+'That\'s right'     # => That's right
+
+"Seconds/day: #{24*60*60}" # => Seconds / day: 86400
+"#{'Ho! ' *3}Merry christmas!" # => Ho! Ho! Ho! Merry CHristmas!
+"Safe level is #$SAFE"    # => Safe level is 0
